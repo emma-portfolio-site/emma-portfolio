0c1c0f2c24bde25b2df0260e8390560660814cbe
emma-portfolio-site <emma.portfolio.site@gmail.com>
2021-10-03 19:17:34 -0500

    first base commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+
+
+
+// CSS
+require('../css/base.css');
+require('../css/vendor.css');
+require('../css/main.css');
+
+window.$ = require('jquery');
+// JS
+require('./plugins.js');
+require('./modernizr.js');
+require('./main.js');
